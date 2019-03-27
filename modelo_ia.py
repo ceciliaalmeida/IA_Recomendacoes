d90fc827fc39029f482506229a0be6e9b17113b6
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2019-03-27 17:23:08 -0300

    Update modelo_ia.py

--- modelo_ia.py
+++ modelo_ia.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pandas as pd
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import load_model
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
 
 # Extrai Respostas dos dados de Treinamento
 # Param 'all_data'  -> Dados Coletados : [cod_curso, cod_mat, nota] (matrix)
 # Return 'ans_data' -> Classificações : [nota] (matrix)
 def extract_answers(all_data):
-  ans_data = data_set[:, 2].astype('int')
+  ans_data = data_set[:, 2].astype('str')
   return ans_data
 
 
@@ -16,7 +19,7 @@ def extract_answers(all_data):
 # Param 'all_data'  -> Dados Coletados : [cod_curso, cod_mat, nota] (matrix)
 # Return 'sensor_data' -> Dados do Historico : [cod_curso, cod_mat] (matrix)
 def extract_data(all_data):
-  subjects_data = data_set[:, 0:2].astype('str')
+  subjects_data = data_set[:, 0:2].astype('int')
   return subjects_data
 
 
@@ -25,19 +28,22 @@ data_set = data_frame.get_values()
 subjects_data = extract_data(data_set)
 ans_data = extract_answers(data_set)
 
-# Crição do Modelo 2-5-1
+encoder = LabelEncoder()
+encoded = encoder.fit_transform(ans_data)
+
+# Crição do Modelo
 model = Sequential()
-model.add(Dense(2, input_dim=2))
-model.add(Dense(5))
-model.add(Dense(1))
+model.add(Dense(6, input_dim=2, activation='relu'))
+model.add(Dense(6, activation='softmax'))
+model.add(Dense(6, activation='softmax'))
 
 # Compilação do Modelo
-model.compile(optimizer='sgd', loss='mse', metrics=['acc'])
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['acc'])
 
 # Treinamento do Modelo
 x = subjects_data
-y = ans_data
-model.fit(x, y, epochs=3000)
+y = np_utils.to_categorical(encoded)
+model.fit(x, y, epochs=10000)
 
 #Salvando Modelo Treinado
 model.save('modelo.h5')
@@ -45,9 +51,9 @@ model.save('modelo.h5')
 while 1==1:
   dados = []
   cod_curso = input('Codigo do Curso')
-  for i in range(0, 77):
+  for i in range(1, 78):
    cod_mat = i
-   dados = [cod_curso,str(cod_mat)]
+   dados = [cod_curso,cod_mat]
    dados = np.asmatrix(dados)
-   nota = model.predict(dados)
-   print(nota[0][0])
+   nota = model.predict_classes(dados)
+   print('Codigo Materia : ' + str(cod_mat) + ' - Nota : ' + str(nota[0]))
