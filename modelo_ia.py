9d415fd91c8200e4a5d44a441e1bf4d500ecbd82
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2019-03-27 16:57:36 -0300

    Update modelo_ia.py

--- modelo_ia.py
+++ modelo_ia.py
@@ -1,34 +1,33 @@
-import numpy
+import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.models import Dense
+from keras.layers import Dense
 from keras.models import load_model
 
 # Extrai Respostas dos dados de Treinamento
-# Param 'all_data'  -> Dados Coletados : [cod_1, cod_2, cod_3, cod_4, cod_5, cod_6] (matrix)
-# Return 'ans_data' -> Classificações : [cod_6] (matrix)
+# Param 'all_data'  -> Dados Coletados : [cod_curso, cod_mat, nota] (matrix)
+# Return 'ans_data' -> Classificações : [nota] (matrix)
 def extract_answers(all_data):
-  ans_data = data_set[:, :5].astype('int')
+  ans_data = data_set[:, 2].astype('int')
   return ans_data
 
 
 # Extrai Valores dos Sensores dos dados de Treinamento
-# Param 'all_data'  -> Dados Coletados : [cod_1, cod_2, cod_3, cod_4, cod_5, cod_6] (matrix)
-# Return 'sensor_data' -> Dados do Historico : [cod_1, cod_2, cod_3, cod_4, cod_5] (matrix)
+# Param 'all_data'  -> Dados Coletados : [cod_curso, cod_mat, nota] (matrix)
+# Return 'sensor_data' -> Dados do Historico : [cod_curso, cod_mat] (matrix)
 def extract_data(all_data):
-  subjects_data = data_set[:, 0:4].astype('int')
+  subjects_data = data_set[:, 0:2].astype('str')
   return subjects_data
 
 
-data_frame = pd.read_csv('dados_coletados.csv')
-data_set = df.values()
+data_frame = pd.read_csv('Perfil_Direito.csv')
+data_set = data_frame.get_values()
 subjects_data = extract_data(data_set)
 ans_data = extract_answers(data_set)
 
-
-# Crição do Modelo 5-5-1
+# Crição do Modelo 2-5-1
 model = Sequential()
-model.add(Dense(5, input_dim=3))
+model.add(Dense(2, input_dim=2))
 model.add(Dense(5))
 model.add(Dense(1))
 
@@ -36,17 +35,19 @@ model.add(Dense(1))
 model.compile(optimizer='sgd', loss='mse', metrics=['acc'])
 
 # Treinamento do Modelo
-x = np.asmatrix(sensor_data)
-y = np.asmaxtrix(ans_data)
+x = subjects_data
+y = ans_data
 model.fit(x, y, epochs=3000)
 
 #Salvando Modelo Treinado
 model.save('modelo.h5')
 
-while true:
+while 1==1:
   dados = []
-  for i in range(0,5):
-    i = int(input('Codigo Historico ' + str(i+1) + ' :'))
-    dados.append(i)
-  dados = np.asmatrix(dados)
-  model.predict(dados)
+  cod_curso = input('Codigo do Curso')
+  for i in range(0, 77):
+   cod_mat = i
+   dados = [cod_curso,str(cod_mat)]
+   dados = np.asmatrix(dados)
+   nota = model.predict(dados)
+   print(nota[0][0])
